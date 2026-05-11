7c7d8cc0aeaf84eeefb6f0d57dd9c93c2e918353
IlianPop <pop.ilian@student.uzhnu.edu.ua>
2026-05-11 13:07:47 +0300

    Реалізація реагування на жести

--- Win64/Debug/test.py
+++ Win64/Debug/test.py
@@ -1,48 +1,34 @@
 import air_mouse
 import cv2
 import mediapipe as mp
+import numpy as np
 detectHand = mp.solutions.hands
+frame_count = 0
 hand = detectHand.Hands(
-    static_image_mode = 0,
+    static_image_mode = False,
     max_num_hands = 1,
+    model_complexity = 0,
     min_detection_confidence = 0.7,
     min_tracking_confidence = 0.4,
 )
+
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 while cap.isOpened():
     ret, frame = cap.read()
-    if ret:
-        frame = cv2.resize(frame, (320, 320))
-        frame = cv2.flip(frame, 1)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame.flags.writeable = False
-        res = hand.process(frame)
-        if res.multi_hand_landmarks:
-            finded_hand = res.multi_hand_landmarks[0].landmark
-            coords = []
-            coords.append(finded_hand[1].x)
-            coords.append(finded_hand[1].y)
-            coords.append(finded_hand[4].x)
-            coords.append(finded_hand[4].y)
-            coords.append(finded_hand[5].x)
-            coords.append(finded_hand[5].y)
-            coords.append(finded_hand[8].x)
-            coords.append(finded_hand[8].y)
-            coords.append(finded_hand[9].x)
-            coords.append(finded_hand[9].y)
-            coords.append(finded_hand[12].x)
-            coords.append(finded_hand[12].y)
-            coords.append(finded_hand[13].x)
-            coords.append(finded_hand[13].y)
-            coords.append(finded_hand[16].x)
-            coords.append(finded_hand[16].y)
-            coords.append(finded_hand[17].x)
-            coords.append(finded_hand[17].y)
-            coords.append(finded_hand[20].x)
-            coords.append(finded_hand[20].y)
-            air_mouse.set_coords(coords)
-        cv2.waitKey(1) & 0xFF
-    else:
-        break
+    if not(ret):break
+    frame = cv2.flip(frame, 1)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame.flags.writeable = False
+    res = hand.process(frame)
+    if res.multi_hand_landmarks:
+        finded_hand = res.multi_hand_landmarks[0].landmark
+        coords = np.array(
+            [val for lmk in finded_hand for val in (lmk.x, lmk.y)]
+            , dtype=np.float64
+        )
+        air_mouse.set_coords(coords.ctypes.data)
 cap.release()
 cv2.destroyAllWindows()
